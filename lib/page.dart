dda41f3d3065528955a9b251d73a10f9e785d8f0
pandazim <jaotoledo74@gmail.com>
2020-05-03 00:37:43 -0400

    Finalizado

--- lib/page.dart
+++ lib/page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
@@ -11,26 +12,46 @@ class Page extends StatefulWidget {
     return _Page();
   }
 }
+const String API_KEY = "7R231W6HL4kRiLfiTKn0JwERHbU6YAxn";
+final String _GifUrl = "http://api.giphy.com/v1/gifs/trending?";
+
+Future<List<Gif>> _recuperaGifs() async {
+  http.Response response;
+  response = await http.get("${_GifUrl}api_key=${API_KEY}&limit=20&rating=G");
+  return compute(parseGifs, response.body);
+}
+
+List<Gif> parseGifs(String responseBody) {
+  var parsed = jsonDecode(responseBody);
+  var gifs = parsed["data"] as List;
+  return gifs.map<Gif>((json) => Gif.fromJson(json)).toList();
+}
+
+class Gif {
+  final String title;
+  final String url;
+
+  Gif({this.title, this.url});
+
+  factory Gif.fromJson(Map<String, dynamic> json) {
+    return Gif(
+      title: json['title'] as String,
+      url: json['images']['fixed_height']['url'] as String
+    );
+  }
+}
 
 class _Page extends State<Page> {
-  final String _GifUrl = "http://api.giphy.com/v1/gifs/trending?";
-  static const String API_KEY = "7R231W6HL4kRiLfiTKn0JwERHbU6YAxn";
 
-  Future<Map> _recuperaGifs() async {
-    http.Response response;
-    response = await http.get("${_GifUrl}api_key=${API_KEY}&limit=20&rating=G");
-    print(response.body);
-    return json.decode(response.body);
-  }
+  Future<List<Gif>> recuperaGifs;
+
+
 
   @override
   void initState() {
     super.initState();
-    _recuperaGifs().then((map) {
-      print(map);
-    });
+    recuperaGifs = _recuperaGifs();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,28 +59,23 @@ class _Page extends State<Page> {
           title: Text("Gifs"),
           backgroundColor: Colors.blue.shade700,
         ),
-        body: Column(
-          children: <Widget>[
-            FutureBuilder(
-              future: _recuperaGifs(),
+        body: Center(
+          child:
+            FutureBuilder<List<Gif>>(
+              future: recuperaGifs,
               builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.done:
-                    return ListView.builder(
-                          itemCount: snapshot.data.lenght,
-                          itemBuilder: (context, index) {
-                            return snapshot.data[index]["title"];
-                          }
-                    );
-                  case ConnectionState.waiting:
-                    print("ll");
-                    break;
-                  case ConnectionState.none:
-                  default:
-                }
+                return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index){
+                      return Image.network(snapshot.data[index].url);
+                    }
+                );
               },
             )
-          ],
+          ,
         ));
   }
 }
